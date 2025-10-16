f178ab779a0d98c13842506b2309f8dc782b1988
Ayush Hirpara <ayushpatel1908@gmail.com>
2025-10-16 12:35:01 +0530

    Add files via upload

---
+++ C Programming/15-10/main (5).c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+    
+    int i,j,space = 0,num;
+    
+    printf("Enter number : ");
+    scanf("%d",&num);
+    
+    for(i=1;i<=num;i++)
+    {
+        for(space=1;space<=num-i;space++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=(2*i-1);j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    getch();
+}
