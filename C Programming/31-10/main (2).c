a6b4bcdc4ad65e2af0eed2ea8a0a7d694fb0d0e2
Ayush Hirpara <ayushpatel1908@gmail.com>
2025-11-01 14:21:58 +0530

    Add files via upload

---
+++ C Programming/31-10/main (2).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+{
+    
+    int data[3][3]={{10,20,30},{40,50,60},{70,80,90}};
+    
+   for(int i=0;i<3;i++)
+   {
+      for(int j=0;j<3;j++)
+      {
+          printf("\n [%d] [%d]=%d",i,j,data[i][j]);
+      }
+    
+   }
+    
+    getch();
+    
+}
